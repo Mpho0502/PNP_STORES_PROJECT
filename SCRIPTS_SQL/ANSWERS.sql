4a119098ce4549015d3f1f3435a3755994a44c1e
Mpho <matebesimoe89@gmail.com>
2026-03-01 22:34:43 +0200

    answers for question 3 & 4

--- SCRIPTS_SQL/ANSWERS.sql
+++ SCRIPTS_SQL/ANSWERS.sql
@@ -7,9 +7,72 @@ Go
 
 --2. Upload 12 CSV files into database.
 
+Select * FROM [PNP_STORES].[dbo].[1772032136184_pnp_sales_2025_01]
+ 
+Select * FROM [PNP_STORES].[dbo].[1772032148926_pnp_sales_2025_02]
 
+Select * FROM [PNP_STORES].[dbo].[1772032159369_pnp_sales_2025_03]
+
+Select * FROM [PNP_STORES].[dbo].[1772032170316_pnp_sales_2025_04]
+
+Select * FROM [PNP_STORES].[dbo].[1772032179739_pnp_sales_2025_05]
+
+Select * FROM [PNP_STORES].[dbo].[1772032189879_pnp_sales_2025_06]
+
+Select * FROM [PNP_STORES].[dbo].[1772032202072_pnp_sales_2025_07]
+
+Select * FROM [PNP_STORES].[dbo].[1772032214258_pnp_sales_2025_08]
+
+Select * FROM [PNP_STORES].[dbo].[1772032224900_pnp_sales_2025_09]
+
+Select * FROM [PNP_STORES].[dbo].[1772032236969_pnp_sales_2025_10]
+
+Select * FROM [PNP_STORES].[dbo].[1772032108507_pnp_sales_2025_11]
+
+Select * FROM [PNP_STORES].[dbo].[1772032261154_pnp_sales_2025_12];
 
 --3. Have a master table where all the data is combined.
+--Data combination
+CREATE VIEW MasterView AS
+Select * FROM [PNP_STORES].[dbo].[1772032136184_pnp_sales_2025_01]
+UNION ALL 
+Select * FROM [PNP_STORES].[dbo].[1772032148926_pnp_sales_2025_02]
+UNION ALL
+Select * FROM [PNP_STORES].[dbo].[1772032159369_pnp_sales_2025_03]
+UNION ALL
+Select * FROM [PNP_STORES].[dbo].[1772032170316_pnp_sales_2025_04]
+UNION ALL
+Select * FROM [PNP_STORES].[dbo].[1772032179739_pnp_sales_2025_05]
+UNION ALL
+Select * FROM [PNP_STORES].[dbo].[1772032189879_pnp_sales_2025_06]
+UNION ALL
+Select * FROM [PNP_STORES].[dbo].[1772032202072_pnp_sales_2025_07]
+UNION ALL
+Select * FROM [PNP_STORES].[dbo].[1772032214258_pnp_sales_2025_08]
+UNION ALL
+Select * FROM [PNP_STORES].[dbo].[1772032224900_pnp_sales_2025_09]
+UNION ALL
+Select * FROM [PNP_STORES].[dbo].[1772032236969_pnp_sales_2025_10]
+UNION ALL
+Select * FROM [PNP_STORES].[dbo].[1772032108507_pnp_sales_2025_11]
+UNION ALL
+Select * FROM [PNP_STORES].[dbo].[1772032261154_pnp_sales_2025_12];
+
+--Master table
+
+SELECT [ProductID]
+      ,[Barcode]
+      ,[ProductName]
+      ,[Category]
+      ,[StoreID]
+      ,[StoreName]
+      ,[Province]
+      ,[CashierName]
+      ,[SaleDate]
+      ,[Quantity]
+      ,[UnitPrice]
+      ,[TotalAmount]
+  FROM [PNP_STORES].[dbo].[MasterView]
 
 --4. Come up with own observations of weird things which you have seen in the data.
 
