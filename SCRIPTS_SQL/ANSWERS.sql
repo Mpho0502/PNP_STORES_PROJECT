ff0e3a2c4a6b835c3eba45073b6bd2fb4ebfcfbc
Mpho <matebesimoe89@gmail.com>
2026-03-01 21:38:19 +0200

    answers for no.1

---
+++ SCRIPTS_SQL/ANSWERS.sql
@@ -0,0 +1,16 @@
+--PNP_STORES_HOMEWORK
+
+--1. Create a database called PnPStore.
+
+Create Database PNP_Stores
+Go
+
+--2. Upload 12 CSV files into database.
+
+
+
+--3. Have a master table where all the data is combined.
+
+--4. Come up with own observations of weird things which you have seen in the data.
+
+--5. Security: Create PnP Data Engineer, Data Analyst, Data Scientist, and Manager Accounts.
