1fa0896266ffbb3f29dc29c1fdd43ceb122636f7
Mpho <matebesimoe89@gmail.com>
2026-03-02 13:09:13 +0200

    Answer for no.5

--- SCRIPTS_SQL/ANSWERS.sql
+++ SCRIPTS_SQL/ANSWERS.sql
@@ -32,6 +32,7 @@ Select * FROM [PNP_STORES].[dbo].[1772032108507_pnp_sales_2025_11]
 Select * FROM [PNP_STORES].[dbo].[1772032261154_pnp_sales_2025_12];
 
 --3. Have a master table where all the data is combined.
+
 --Data combination
 CREATE VIEW MasterView AS
 Select * FROM [PNP_STORES].[dbo].[1772032136184_pnp_sales_2025_01]
@@ -59,7 +60,6 @@ UNION ALL
 Select * FROM [PNP_STORES].[dbo].[1772032261154_pnp_sales_2025_12];
 
 --Master table
-
 SELECT [ProductID]
       ,[Barcode]
       ,[ProductName]
@@ -77,3 +77,20 @@ SELECT [ProductID]
 --4. Come up with own observations of weird things which you have seen in the data.
 
 --5. Security: Create PnP Data Engineer, Data Analyst, Data Scientist, and Manager Accounts.
+
+USE [master]
+GO
+
+/* For security reasons the login is created disabled and with a random password. */
+
+CREATE LOGIN [Ora_PnP_Data_Engineer] WITH PASSWORD=N'zIo3xCuN414OUOctk9vcGOHZQa+PY1HjupedPTm+UVs=', DEFAULT_DATABASE=[PNP_STORES], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
+GO
+
+CREATE LOGIN [Mpho_PnP_Data_Analyst] WITH PASSWORD=N'OhG/JtCeF7mt1XCQDUDCmA14TM0//WV9pp04AYnTS5I=', DEFAULT_DATABASE=[PNP_STORES], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
+GO
+
+CREATE LOGIN [Enid_PnP_Data_Scientist] WITH PASSWORD=N'ousPsbGIjKPYJzmnoOSHTpRy35y5Eofk8RWjjkus07s=', DEFAULT_DATABASE=[PNP_STORES], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
+GO
+
+CREATE LOGIN [Mat_PnP_Manager] WITH PASSWORD=N'UogacYoXpsHB8Wr1zNuYuzUkqPi4UBW49r4mHWYqp2Q=', DEFAULT_DATABASE=[PNP_STORES], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
+GO
